5975e2662d9d64c991debd76045899f9b7d8883f
Maulana Adam Sahid <masadamsahid@gmail.com>
2025-04-04 05:32:15 +0700

    Init home page and setup firebase

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
 type Props = {
   children: React.ReactNode;
 }
 
 const RootLayout = ({ children }: Props) => {
   return (
-    <div>
+    <div className="root-layout">
+      <nav>
+        <Link href='/' className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" width={38} height={32} />
+          <h2 className="text-primary-100">Prep-In</h2>
+        </Link>
+      </nav>
       {children}
     </div>
   );
