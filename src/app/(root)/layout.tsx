2df8c99bcde7164c02f3d6b5f4db1c58d8b329a1
Maulana Adam Sahid <masadamsahid@gmail.com>
2025-04-03 15:20:22 +0700

    initial files and initial route grouping

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  children: React.ReactNode;
+}
+
+const RootLayout = ({ children }: Props) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
+
+export default RootLayout;
